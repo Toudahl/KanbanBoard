a8f8c3dcf29823f890adb3568f8b7c70f4d635c0
Toudahl <toudahl@protonmail.ch>
2015-02-04 13:43:17 +0100

    Partial commit with doxygen generated documentation

---
+++ Documentation/html/class_kanban_board_1_1_view_model_1_1_main_view_model.js
@@ -0,0 +1,19 @@
+var class_kanban_board_1_1_view_model_1_1_main_view_model =
+[
+    [ "DragOver", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#afeac47ff8c81ee187966b321dcd0adfd", null ],
+    [ "Drop", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a8a289987889845510435516f3699495c", null ],
+    [ "LoadFromDialog", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#aeb9121b810fcc08bc9fbcc26c02092b5", null ],
+    [ "NewBoard", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a7762a9f2331ec0b68d773570ac185ffc", null ],
+    [ "SaveAsDialog", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#abdee48fdc9e1e61ffe046dc1d13c59dd", null ],
+    [ "SaveBoard", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#aa9116851f4b8e55c05045900745e4c39", null ],
+    [ "CompletedCategory", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#aa3cb439ea891ec13ad4ab256efa0fc2a", null ],
+    [ "ListOfCompletedWork", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a6ecd7d9718b6e320441c5938dc1c01eb", null ],
+    [ "ListOfToDo", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a250c79012624be5145f7a08ce9fdd4d8", null ],
+    [ "ListOfWorkInProgress", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#ac28a6f8225d24e60ea7f1196cbf8d6a3", null ],
+    [ "LoadFromDialogCommand", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#ac78c41a4e5c852c2afdd853439def3b0", null ],
+    [ "NewCommand", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#ac8874b3c893afb269198991fb7780d3e", null ],
+    [ "SaveAsDialogCommand", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a67c109d76784dcd84a0b3601689518e9", null ],
+    [ "SaveCommand", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#ae8b5f8979602622e17bc76d377c8b4c2", null ],
+    [ "ToDoCategory", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#a4896420588efd2aad29351a8d8d53657", null ],
+    [ "WorkInProgressCategory", "class_kanban_board_1_1_view_model_1_1_main_view_model.html#abab8ec3a18854b226218782bcbee971b", null ]
+];
---
+++ Documentation/html/class_kanban_board_1_1_view_model_1_1_manipulate_post_it_view_model.js
@@ -0,0 +1,4 @@
+var class_kanban_board_1_1_view_model_1_1_manipulate_post_it_view_model =
+[
+    [ "Save", "class_kanban_board_1_1_view_model_1_1_manipulate_post_it_view_model.html#a5dfdcefe05008e962b03e87a9887b7ec", null ]
+];
